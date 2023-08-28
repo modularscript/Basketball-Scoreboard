d51e4ddf81ee9d72c92daf4fcbe1e6c155932d30
modularscript <9781533+modularscript@users.noreply.github.com>
2023-08-28 19:47:44 +0200

    Buttons

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let homePoints = 0
-let guestPoints = 0
+let homeStartScore = 0
+let guestStartScore = 0
 let onePoint = 1
 let twoPoints = 2
 let threePoints = 3
@@ -8,5 +8,34 @@ const homeScore = document.getElementById('home-score')
 const guestScore = document.getElementById('guest-score')
 
 
-// homeScore.textContent = 1
-// guestScore.textContent = 155
+function addHome1() {
+  homeStartScore += 1;
+  homeScore.textContent = homeStartScore;
+}
+
+function addHome2() {
+  homeStartScore += 2;
+  homeScore.textContent = homeStartScore;
+}
+
+function addHome3() {
+  homeStartScore += 3;
+  homeScore.textContent = homeStartScore;
+}
+
+
+
+function addGuest1() {
+  guestStartScore += 1;
+  guestScore.textContent = guestStartScore;
+}
+
+function addGuest2() {
+  guestStartScore += 2;
+  guestScore.textContent = guestStartScore;
+}
+
+function addGuest3() {
+  guestStartScore += 3;
+  guestScore.textContent = guestStartScore;
+}
