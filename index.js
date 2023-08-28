3bc17a2c40e8973dde8c18b8242bd967dc9a5c22
modularscript <9781533+modularscript@users.noreply.github.com>
2023-08-28 17:37:40 +0200

    Update in js file

---
+++ index.js
@@ -0,0 +1,12 @@
+let homePoints = 0
+let guestPoints = 0
+let onePoint = 1
+let twoPoints = 2
+let threePoints = 3
+
+const homeScore = document.getElementById('home-score')
+const guestScore = document.getElementById('guest-score')
+
+
+// homeScore.textContent = 1
+// guestScore.textContent = 155
